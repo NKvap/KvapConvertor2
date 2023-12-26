db04c0faeacfd8346e1bd56639436dce43d654bc
altaee14 <altaeee@yandex.ru>
2023-12-27 00:46:21 +0500

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace KvapConvertor;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using KvapConvertor;
+
+namespace KvapConvertor;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+		BindingContext = new ConverterMainViewModel();
+	}
+}
+
